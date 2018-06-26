3cac101559e125c0351b3001b3df90865ee584c5
Josh Tucholski <josh@techelevator.com>
2018-06-26 16:29:17 +0000

    Program.cs edited online with Bitbucket

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -11,10 +11,7 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            // Sample Code to get a connection string from the
-            // App.Config file
-            // Use this so that you don't need to copy your connection string all over your code!
-            string connectionString = ConfigurationManager.ConnectionStrings["CapstoneDatabase"].ConnectionString;
+            
         }
     }
 }
