8d6fde5f87e4662e9b8e4a68e156d26c15e4589b
William Lawrence <williamjlawrence1@gmail.com>
2018-06-27 10:28:48 -0400

    added models

---
+++ Capstone/Models/Campground.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+     public class Campground
+    {
+        public int CampgroundID { get; set; }
+
+        public int ParkID { get; set; } 
+
+        public string Name { get; set; }
+
+        public int OpeningMonth { get; set; }
+
+        public int ClosingMonth { get; set; }
+
+        public decimal DailyFee { get; set; }
+    }
+}
