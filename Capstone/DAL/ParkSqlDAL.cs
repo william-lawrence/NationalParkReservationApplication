8f3bf2e3d80e23cca524cea4d132a6f2f9cc86b2
Sarah Robertson <robertson_sarah@yahoo.com>
2018-06-27 09:55:08 -0400

    Started CLI & ParkDAL

---
+++ Capstone/DAL/ParkSqlDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.DAL
+{
+	public class ParkSqlDAL
+	{
+		private string connectionString;
+
+		public ParkSqlDAL(string dbConnectionString)
+		{
+			connectionString = dbConnectionString;
+		}
+
+		public IList<Park> GetAllParks()
+		{
+			IList<Park> output = new List<Park>();
+		}
+	}
+}
